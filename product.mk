3536130fa7257b3ea8cfa15b98308f13cc785a2b
Sarthak Roy <sarthakroy2002@gmail.com>
2021-10-20 08:34:15 +0800

    extra-overlay: Set animation/transition scale to 50%
    - Feels way smoother
    
    Change-Id: I158ca53f12596e5cbd56fc4c35ca54d76d6ae835
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- product.mk
+++ product.mk
@@ -6,6 +6,9 @@
 
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
