0741dad322f645cb6ded96e61ba4d3a67885e40c
John Galt <johngaltfirstrun@gmail.com>
2025-02-09 11:32:38 -0500

    extra-product: Override dexopt flags/props
    * Since the time hit isn't severe here, always optimize.
    
    Change-Id: I62ad0bdfd050d3e2c296994a6cd477aad377a958

--- properties.mk
+++ properties.mk
@@ -1,3 +1,10 @@
+# Dexopt
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    pm.dexopt.first-boot=verify \
+    pm.dexopt.bg-dexopt=speed \
+    pm.dexopt.post-boot=verify \
+    pm.dexopt.shared=speed
+
 # Disable blur on app launch
 PRODUCT_VENDOR_PROPERTIES += \
     ro.launcher.blur.appLaunch=0
