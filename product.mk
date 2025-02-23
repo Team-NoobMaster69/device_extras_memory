d7407f64783ce811efb282faf855ca4a9b63894a
Park Ju Hyung <qkrwngud825@gmail.com>
2025-02-23 19:31:29 -0300

    extras-memory: Optimize everything on preopt
    We have plenty of space left at /system
    
    Change-Id: I281651eb487e2b59d645cdde1943db6c08fb6d02
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- product.mk
+++ product.mk
@@ -12,5 +12,8 @@ PRODUCT_PACKAGES += init.device.perf.rc
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Optimize everything for preopt
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+
 # Properties
 include $(LOCAL_PATH)/properties/common.mk
